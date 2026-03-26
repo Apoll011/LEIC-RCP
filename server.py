7dac4f7e024e3392d8665c51620ea0d12e5e40dc
apoll011 <tiagorobotik@gmail.com>
2026-03-26 19:52:40 +0000

    refactor: improve HTTPServer class structure and error handling; move index.html to test directory

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import io
 import os
 import socket
 
+# ALgums Dicionarios com valors a serem usados
 http_responses = {
     200: "OK",
     404: "Not Found",
@@ -27,36 +28,34 @@ content_types = {
 }
 
 
+#Cria um classe servidor
 class HTTPServer:
-    def __init__(
-        self, 
-        port: int = 80, 
-        path: str = "./web"
-    ) -> None:
-        self.path = path
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.sock.bind(('0.0.0.0', port))
+    def __init__(self, port: int = 80, path: str = "./web") -> None:
+        self.base_path = path
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       # Cria a conexção socket
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)     # Define a conexção como TCP
+        self.sock.bind(('0.0.0.0', port))                                   # A conexcao faz bind na porta pedida
 
-        self.sock.listen()
+        self.sock.listen()                                                  # COmeça a escutar por pedidos de conexção
 
     def loop(self) -> None:
         while True:
-            conn, addr = self.sock.accept()
+            conn, _ = self.sock.accept()                                    # Cliente conectou
 
             with conn:
-                request_stream = conn.makefile('rb')
+                request_stream = conn.makefile('rb')                        # Crie arquivos virtuais para leitura e escrita dos dados
                 response_stream = conn.makefile('wb')
-                self.handle(
-                    stream=request_stream,
-                    response_stream=response_stream
-                )
+                try: 
+                    self.handle(request_stream, response_stream)            # Pedido do Cliente
+                except Exception as e:
+                    self.error(response_stream, e)
+                conn.close()
 
     def parse_request(self, stream: io.BufferedIOBase):
-        request = stream.readline().decode().rstrip('\r\n')
+        request = stream.readline().decode().rstrip('\r\n').split(' ')
 
-        request_type = request.split(' ')[0]
-        path = request.split(' ')[1]
+        request_type = request[0]
+        path = request[1]
 
         headers = {} 
         line = stream.readline().decode()
@@ -65,25 +64,42 @@ class HTTPServer:
             headers[header[0]] = header[1]
             line = stream.readline().decode()
         
-        if path == "/":
-            path = "/index.html"
+        full_path = self.base_path + path
 
-        return request_type, path, headers
+        if (os.path.isdir(full_path)):
+            full_path += "/index.html"
+
+        return request_type, full_path, headers
+
+    def build_response(self, path):
+        extensao = path.split(".")[-1]
+
+        content_type = content_types[extensao]
+
+        headers = {
+            "Content-Type": content_type,
+            "Content-Length": os.path.getsize(path),
+            "Connection": "close"
+        }
+
+        with open(path, "rb") as file:
+            content = file.read()
+
+        return headers, content
+
 
     def handle(self, stream: io.BufferedIOBase, response_stream: io.BufferedIOBase):
-        type, path, headers = self.parse_request(stream)
+        type, path, _ = self.parse_request(stream)
         
         if type != "GET":
             self.not_implemented(response_stream)
             return
-
-        full_path = self.path + path
-
-        if not self.exists(full_path):
+        
+        if not os.path.exists(path):
             self.not_found(response_stream, path)
             return
         
-        response_headers, content = self.response(full_path)
+        response_headers, content = self.build_response(path)
         self.send_to_client(response_stream, 200, response_headers, content)
 
     def not_found(self, write_stream: io.BufferedIOBase, path: str):
@@ -95,7 +111,7 @@ class HTTPServer:
   <head><title>404 Not Found</title></head>
   <body>
     <h1>Not Found</h1>
-    <p>O caminho {path} não foi encontrado.</p>
+    <p>O caminho {path.lstrip(self.base_path)} não foi encontrado.</p>
   </body>
 </html>""", "utf-8"))
     
@@ -113,6 +129,21 @@ class HTTPServer:
   </body>
 </html>
 """, "utf-8"))
+    
+    def error(self, write_stream: io.BufferedIOBase, e: Exception): 
+        self.send_to_client(write_stream, 500, {
+            "Content-Type": "text/html; charset=UTF-8",
+            "Content-Length": 139 + len(str(e)),
+            "Connection": "close"
+        }, bytes(f"""
+<html>
+  <head><title>500 Server Error</title></head>
+  <body>
+    <h1>Server Error</h1>
+    <p>Occorreu um erro: {str(e)}</p>
+  </body>
+</html>
+""", "utf-8"))
         
     def send_to_client(self, write_stream: io.BufferedIOBase, code: int, headers: dict[str, str|int], content: bytes):
         headers_str = "".join([f"{key}: {value}\r\n" for key, value in headers.items()])
@@ -122,32 +153,12 @@ class HTTPServer:
         write_stream.write(bytes(resp_content, "utf-8") + content)
         write_stream.flush()
 
-    def exists(self,path):
-        return os.path.exists(path)
-
-    #./web/index.html, ./web/icon.png
-    def response(self, path):
-        extensao = path.split(".")[-1]
-
-        content_type = content_types[extensao]
-
-        headers = {
-            "Content-Type": content_type,
-            "Content-Length": os.path.getsize(path),
-            "Connection": "close"
-        }
-
-        with open(path, "rb") as file:
-            content = file.read()
-
-        return headers, content
-
-    def __enter__(self) -> 'HTTPServer':
-        return self
-
-    def __exit__(self, *args) -> None:
+    def close(self, *args) -> None:
         self.sock.close()
 
 if __name__ == "__main__":
-    with HTTPServer(8100) as server:
+    server = HTTPServer(8100)
+    try:
         server.loop()
+    except KeyboardInterrupt:
+        server.close()
